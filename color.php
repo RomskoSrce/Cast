a0676f5e742024bfc91b61373c9e2103791944af
RomskoSrce <66077769+RomskoSrce@users.noreply.github.com>
2022-11-04 23:09:10 +0100

    Add files via upload

---
+++ color.php
@@ -0,0 +1,13 @@
+.btn--base , .custom--tab .nav-item .nav-link.active , .btn--base:hover , .rj-single .thumb .social-links li a , .social-links-list li a , .scroll-to-top , .audio-player.style--two .play-container, .pagination .page-item.active .page-link , .date-select-form .date-select-btn,.ui-state-highlight, .ui-widget-content .ui-state-highlight, .ui-widget-header .ui-state-highlight{
+background:#cc2e94}
+.text--base , .footer-short-list li a:hover , .preloader__inner .logo-area .logo-name{
+color: #cc2e94 !important}
+
+.controls .right{
+background-color : #cc2e94}
+
+.custom--tab .nav-item .nav-link.active,.ui-state-highlight, .ui-widget-content .ui-state-highlight, .ui-widget-header .ui-state-highlight{
+border-color: #cc2e94}
+
+.header.menu-fixed .header__bottom , .header .site-logo, .footer::before{
+background-color:#0D2F4D !important}
